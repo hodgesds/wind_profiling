077662bc3129f2e29bfe50ce5b4e1078877e65f5
Andrew Borgman <borgmaan@mail.gvsu.edu>
2013-01-11 11:59:42 -0500

    Data checker

---
+++ python/data_checking.py
@@ -0,0 +1,14 @@
+# Checking to see if all data from One_Second_Data is in same format
+import os,sys
+
+data_dir = "/home/borgmaan/ws_gvsu/Data/One_Second_Data/"
+
+folders = [data_dir + x + "/" for x in os.listdir(data_dir)]
+
+for f in folders:
+	folder_files = [f + x for x in os.listdir(f)]
+	for l in folder_files:
+		with open(l) as infile:
+			for line in infile:
+				spl = line.split(",")
+				print len(spl)
