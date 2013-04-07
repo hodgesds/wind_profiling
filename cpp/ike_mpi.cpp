1347614c7b72917262b8e42005385bbb81c8563b
user <no.email>
2013-04-06 22:22:39 -0400

    omp_ike finished

--- cpp/ike_mpi.cpp
+++ cpp/ike_mpi.cpp
@@ -16,62 +16,60 @@
 //g++ -fopenmp hw7_3.cpp 
 #include <iostream>
 #include <stdio.h>
+#include <cstdlib>
 #include <stdlib.h>
 #include <omp.h>
-#include <sys/types.h>
-#include <dirent.h>
-#include <errno.h>
-#include <vector>
 #include <sstream>
-#include <string>
 #include <fstream>
-#include <math.h>
- #include <string.h>
+#include <string>
+#include <stdlib.h>
+#include <vector>
+#include <cmath>
+#include <cstring>
+#include <string.h>
 using namespace std;
 #define OMP_STACKSIZE 15G
 // default stack size for openmp sucks
 // type this in bash so you stop getting overflows
 // ulimit -s unlimited
 
-// known height of 3 meters
-double known_height = 3.0;
-
-float calc_coefficient(float obs_z, float height_z, float obs_ref, float height_ref)
+double calc_coefficient(double obs_z, double height_z, double obs_ref, double height_ref)
 {
-    float height_ratio,speed_ratio,alpha;
+    double height_ratio = 0;
+    double speed_ratio = 0;
+    double alpha = 0;
     height_ratio = height_z / height_ref;
     speed_ratio = obs_z / obs_ref;
     alpha =  log(speed_ratio)/log(height_ratio);
-	return alpha;
+    return alpha;
 }
 
-
-
 int main(int argc, const char *argv[]) {
-    // Get stdin...
+    // Get stdin...+9
     if (argc < 3) 
     {
 		cerr << "usage: ./progName infile.tsv outfile.tsv nthreads\n" << endl;
 		exit(-1);
 	}
     ifstream infile(argv[1]);
+    cout << argv[1] << endl;
     // open the file
 	if (!infile) {
 		cerr << "file not found" << endl;
 		exit(-1);
 	}
     // open output file
-    //ofstream outfile;
-    //outfile.open(argv[2]);
-    //if( !outfile ) 
-    //{ 
-    //    cerr << "Error: output file could not be opened" << endl;
-    //    exit(1);
-    //}
+    ofstream outfile;
+    outfile.open(argv[2]);
+    if( !outfile ) 
+    { 
+        cerr << "Error: output file could not be opened" << endl;
+        exit(1);
+    }
     // variables for doing some work
     int nthreads = atoi(argv[3]);
+    double known_height = 3.0;
     string line;
-    vector<std::string> line_data;
     string timestamp;
     vector<double> heights; 
     heights.push_back(75.0);
@@ -80,25 +78,65 @@ int main(int argc, const char *argv[]) {
     heights.push_back(125.0);
     heights.push_back(150.0);
     heights.push_back(175.0);
-    double results;
-    while (getline(infile, line, '\t'))
+    string partial;
+    vector<string> bigass_data;
+    vector<string> results;
+    while(getline(infile, line)) 
+    { 
+        bigass_data.push_back(line);
+        results.push_back(""); // make a empty results vector... wait for the magic.
+    }
+    int a=0;
+    size_t pos = 0;
+    int splits = 0;
+    int i = 0;
+    double k1_speed,k2_speed,o1_speed,r_one,r_two;
+    char jimmy_buffer [50];
+    string buffer_slip;
+    string outkast; // i love the way you move
+    string token;
+    vector<string> tokens;
+    string delimiter = "\t";
+    #pragma omp parallel for num_threads(nthreads)  private(a,i,outkast,jimmy_buffer,buffer_slip,r_one,r_two,pos,splits,o1_speed,k1_speed,k2_speed,tokens,token)
+    for (a=0; a<bigass_data.size(); ++a)
     {
-        line_data.push_back(line);
-        timestamp = line_data[0];
-        cout << heights[0] << "\n";
-        int i=0;
-        #pragma omp parallel for num_threads(nthreads) private(i,results)
-        for (i=0; i<6; ++i)
+        
+        splits = 0;
+        pos = 0;
+        while ((pos = bigass_data[a].find(delimiter)) != string::npos) 
         {
-            results = calc_coefficient( atof(line_data[i+1].c_str()), heights[i], atof(line_data[7].c_str()), known_height);
-            //#pragma omp critical
-            //{
-            //    cout << results  << "\n";
-            //    //outfile << timestamp << "\t" << results << "\t" << estimated_height << "\n";
-            //    //results = 1;
-            //}
+            token = bigass_data[a].substr(0, pos);
+            splits +=1;
+            tokens.push_back(token);
+            //cout << token << ' ' << pos << endl;
+            bigass_data[a].erase(0, pos + delimiter.length());
         }
+        if (splits == 8)
+        {
+            //cout << tokens[tokens.size()-1] << "-" << tokens[8] << '\n';
+            i = 0;
+            for (i=1; i<6; ++i)
+            {
+                o1_speed = atof(tokens[i].c_str() );
+                k1_speed = atof(tokens[6].c_str() ); 
+                k2_speed = atof(tokens[7].c_str() ); 
+                r_one = calc_coefficient( o1_speed, heights[i-1], k1_speed, known_height);
+                r_two = calc_coefficient( o1_speed, heights[i-1], k2_speed, known_height);
+            }
+                sprintf(jimmy_buffer, "\t%f\t%f\n",r_one,r_two);
+                buffer_slip = jimmy_buffer;
+                outkast = tokens[0] + buffer_slip;
+                results[a] = outkast;
+                //outfile << tokens[0] << '\t' << r_one << '\t' << r_two << '\n';
+        }
+
+        tokens.clear();
     }     
-    //outfile.close();
+    for (a=0; a<results.size(); ++a)
+    {
+        outfile << results[a] ;
+    }
+    
+    outfile.close();
     return 0;
 }
